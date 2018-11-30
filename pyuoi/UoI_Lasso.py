244ec034bc36f45fe01d8dfdb421f3d66a3c9997
Jesse Livezey <jesse.livezey@gmail.com>
2018-11-30 15:26:16 -0800

    moved stability_selection_to_threshold out of UoILasso

--- pyuoi/UoI_Lasso.py
+++ pyuoi/UoI_Lasso.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import check_X_y
 
 from pyuoi import utils
+from .selection import stability_selection_to_threshold
 
 
 class UoI_Lasso(LinearModel, SparseCoefMixin):
@@ -279,80 +280,6 @@ class UoI_Lasso(LinearModel, SparseCoefMixin):
 
         return self
 
-    def stability_selection_to_threshold(self, stability_selection):
-        """Converts user inputted stability selection to an array of
-        thresholds. These thresholds correspond to the number of bootstraps
-        that a feature must appear in to guarantee placement in the selection
-        profile.
-
-        Parameters
-        ----------
-        stability_selection : int, float, or array-like
-            If int, treated as the number of bootstraps that a feature must
-            appear in to guarantee placement in selection profile. If float,
-            must be between 0 and 1, and is instead the proportion of
-            bootstraps. If array-like, must consist of either ints or floats
-            between 0 and 1. In this case, each entry in the array-like object
-            will act as a separate threshold for placement in the selection
-            profile.
-        """
-
-        # single float, indicating proportion of bootstraps
-        if isinstance(stability_selection, float):
-            selection_thresholds = np.array([int(
-                stability_selection * self.n_boots_sel
-            )])
-
-        # single int, indicating number of bootstraps
-        elif isinstance(stability_selection, int):
-            selection_thresholds = np.array([int(
-                stability_selection
-            )])
-
-        # list, to be converted into numpy array
-        elif isinstance(stability_selection, list):
-            # list of floats
-            if all(isinstance(idx, float) for idx in stability_selection):
-                selection_thresholds = \
-                    self.n_boots_sel * np.array(stability_selection)
-
-            # list of ints
-            elif all(isinstance(idx, int) for idx in stability_selection):
-                selection_thresholds = np.array(stability_selection)
-
-            else:
-                raise ValueError("Stability selection list must consist of "
-                                 "floats or ints.")
-
-        # numpy array
-        elif isinstance(stability_selection, np.ndarray):
-            # np array of floats
-            if np.issubdtype(stability_selection.dtype.type, np.floating):
-                selection_thresholds = self.n_boots_sel * stability_selection
-
-            # np array of ints
-            elif np.issubdtype(stability_selection.dtype.type, np.integer):
-                selection_thresholds = stability_selection
-
-            else:
-                raise ValueError("Stability selection array must consist of "
-                                 "floats or ints.")
-
-        else:
-            raise ValueError("Stability selection must be a valid float, int "
-                             "or array.")
-
-        # ensure that ensuing list of selection thresholds satisfies
-        # the correct bounds
-        selection_thresholds = selection_thresholds.astype('int')
-        if not (
-            np.all(selection_thresholds <= self.n_boots_sel) and
-            np.all(selection_thresholds > 1)
-        ):
-            raise ValueError("Stability selection thresholds must be within "
-                             "the correct bounds.")
-
-        return selection_thresholds
 
     def intersection(self, coefs):
         """Performs the intersection operation on selection coefficients
@@ -366,8 +293,8 @@ class UoI_Lasso(LinearModel, SparseCoefMixin):
         """
 
         # extract selection thresholds from user provided stability selection
-        self.selection_thresholds = self.stability_selection_to_threshold(
-            stability_selection=self.stability_selection
+        self.selection_thresholds = stability_selection_to_threshold(
+            self.stability_selection, self.n_boots_sel
         )
 
         # create support matrix
--- pyuoi/tests/test_uoi_lasso.py
+++ pyuoi/tests/test_uoi_lasso.py
@@ -2,15 +2,16 @@ import numpy as np
 from numpy.testing import assert_array_equal
 
 from pyuoi.UoI_Lasso import UoI_Lasso
+from pyuoi.selection import stability_selection_to_threshold
 
 
 def test_stability_selection_to_threshold():
+    """Test different input types for stability selection."""
     n_boots_sel = 48
-    uoi = UoI_Lasso(n_boots_sel=n_boots_sel)
 
     # stability selection is a single float
     test_float = 0.5
-    selection_thresholds = uoi.stability_selection_to_threshold(test_float)
+    selection_thresholds = stability_selection_to_threshold(test_float, n_boots_sel)
     assert_array_equal(
         selection_thresholds,
         np.array([int(test_float * n_boots_sel)])
@@ -18,7 +19,7 @@ def test_stability_selection_to_threshold():
 
     # stability selection is a single integer
     test_int = 36
-    selection_thresholds = uoi.stability_selection_to_threshold(test_int)
+    selection_thresholds = stability_selection_to_threshold(test_int, n_boots_sel)
     assert_array_equal(
         selection_thresholds,
         np.array([test_int])
@@ -26,7 +27,7 @@ def test_stability_selection_to_threshold():
 
     # stability selection is a list of floats
     test_floats = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    selection_thresholds = uoi.stability_selection_to_threshold(test_floats)
+    selection_thresholds = stability_selection_to_threshold(test_floats, n_boots_sel)
     print((n_boots_sel * np.array(test_floats)).astype('int'))
     assert_array_equal(
         selection_thresholds,
