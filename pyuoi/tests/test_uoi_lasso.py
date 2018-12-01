1ff7352423ad796ca5b2d626d5c3cbcd8570eeba
Andrew Tritt <ajtritt@lbl.gov>
2018-11-30 17:06:55 -0800

    cleanup tests

--- pyuoi/tests/test_uoi_lasso.py
+++ pyuoi/tests/test_uoi_lasso.py
@@ -9,28 +9,18 @@ def test_stability_selection_to_threshold_float():
     # stability selection is a single float
     test_float = 0.5
     selection_thresholds = UoI_Lasso._stability_selection_to_threshold(test_float, n_boots_sel)
-    assert_array_equal(
-        selection_thresholds,
-        np.array([int(test_float * n_boots_sel)])
-    )
+    assert_array_equal(selection_thresholds, [24])
 
 def test_stability_selection_to_threshold_int():
     n_boots_sel = 48
     # stability selection is a single integer
     test_int = 36
     selection_thresholds = UoI_Lasso._stability_selection_to_threshold(test_int, n_boots_sel)
-    assert_array_equal(
-        selection_thresholds,
-        np.array([test_int])
-    )
+    assert_array_equal(selection_thresholds, [36])
 
 def test_stability_selection_to_threshold_floats():
     n_boots_sel = 48
     # stability selection is a list of floats
     test_floats = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     selection_thresholds = UoI_Lasso._stability_selection_to_threshold(test_floats, n_boots_sel)
-    print((n_boots_sel * np.array(test_floats)).astype('int'))
-    assert_array_equal(
-        selection_thresholds,
-        (n_boots_sel * np.array(test_floats)).astype('int')
-    )
+    assert_array_equal(selection_thresholds, [24, 28, 33, 38, 43, 48])
