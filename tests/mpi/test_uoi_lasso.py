676b003a42b4e1517163682faee4ee5b8cb40790
Andrew Tritt <ajtritt@lbl.gov>
2018-12-06 16:57:06 -0800

    add tests for MPI implementation

---
+++ tests/mpi/test_uoi_lasso.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy.testing import assert_array_equal, assert_array_almost_equal_nulp
+
+from pyuoi import UoI_Lasso
+from mpi4py import MPI
+
+def test_variable_selection():
+    """Test basic functionality of UoI_Lasso and that it finds the write model"""
+    from sklearn.datasets import make_regression
+    X, y, w = make_regression(coef=True, random_state=1)
+    lasso = UoI_Lasso(comm=MPI.comm)
+    lasso.fit(X, y)
+    true_coef = np.nonzero(w)[0]
+    fit_coef = np.nonzero(lasso.coef_)[0]
+    assert_array_equal(true_coef, fit_coef)
+    assert_array_almost_equal_nulp(true_coef, fit_coef)
+
+
+if __name__ == '__main__':
+    test_variable_selection()
