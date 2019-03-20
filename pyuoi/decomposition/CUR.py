87bf3a04808e03df23b925c322769b66e69d1767
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2019-03-20 08:42:36 -0700

    Column select

--- pyuoi/decomposition/CUR.py
+++ pyuoi/decomposition/CUR.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator
+from sklearn.decomposition import TruncatedSVD
 import numpy as np
 
 
@@ -13,3 +14,21 @@ class CUR(BaseEstimator):
 
     def fit(self, X):
         return
+
+    def column_select(V, c):
+        """Chooses column indices from a matrix given its SVD."""
+        # extract number of samples and rank
+        n_features, k = V.shape
+
+        # calculate normalized leverage score
+        pi = np.sum(V**2, axis=1) / k
+
+        # iterate through columns
+        column_flags = np.zeros(n_features, dtype=bool)
+
+        for column in range(n_features):
+            p = min(1, c * pi[column])
+
+            column_flags[column] = p > np.random.rand()
+
+        return column_flags
