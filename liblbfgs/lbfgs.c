ba6b1f094eddb9e21574118b9e8a75ac5e54fd04
Jesse Livezey <jlivezey@lbl.gov>
2019-04-08 16:40:16 -0700

    fixed abs bug

--- liblbfgs/lbfgs.c
+++ liblbfgs/lbfgs.c
@@ -132,7 +132,7 @@ typedef int (*line_search_proc)(
     callback_data_t *cd,
     const lbfgs_parameter_t *param
     );
-    
+
 static int line_search_backtracking(
     int n,
     lbfgsfloatval_t *x,
@@ -510,7 +510,7 @@ int lbfgs(
         /*
             Test for stopping criterion.
             The criterion is given by the following formula:
-                (f(past_x) - f(x)) / f(x) < \delta
+                |f(past_x) - f(x)| / f(x) < \delta
          */
         if (pf != NULL) {
             /* We don't test the stopping criterion while k < past. */
@@ -519,7 +519,7 @@ int lbfgs(
                 rate = (pf[k % param.past] - fx) / fx;
 
                 /* The stopping criterion. */
-                if (rate < param.delta) {
+                if (fabs(rate) < param.delta) {
                     ret = LBFGS_STOP;
                     break;
                 }
@@ -1116,7 +1116,7 @@ static int line_search_morethuente(
  *  @param  brackt  The pointer to the predicate if the trial value is
  *                  bracketed.
  *  @retval int     Status value. Zero indicates a normal termination.
- *  
+ *
  *  @see
  *      Jorge J. More and David J. Thuente. Line search algorithm with
  *      guaranteed sufficient decrease. ACM Transactions on Mathematical
@@ -1248,7 +1248,7 @@ static int update_trial_interval(
             x <- x, y <- t.
         - Case b: if f(t) <= f(x) && f'(t)*f'(x) > 0,
             x <- t, y <- y.
-        - Case c: if f(t) <= f(x) && f'(t)*f'(x) < 0, 
+        - Case c: if f(t) <= f(x) && f'(t)*f'(x) < 0,
             x <- t, y <- x.
      */
     if (*fx < *ft) {
