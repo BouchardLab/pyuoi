04c06ed4a6f777061dc976460580b31412b0a894
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2019-04-17 14:51:32 -0700

    Initial NMF tests.

--- pyuoi/decomposition/__init__.py
+++ pyuoi/decomposition/__init__.py
@@ -2,7 +2,9 @@
 from .CUR import UoI_CUR
 from .CUR import CUR
 from .NMF import UoI_NMF
+from .NMF import UoI_NMF_Base
 
 __all__ = ["UoI_CUR",
            "CUR",
-           "UoI_NMF"]
+           "UoI_NMF",
+           "UoI_NMF_Base"]
