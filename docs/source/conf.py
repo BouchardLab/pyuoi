befd7c819a5d304ea155c79e74b5fdb4fa8d5e65
Andrew Tritt <ajtritt@lbl.gov>
2019-09-18 15:41:42 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,9 @@ import sphinx_rtd_theme
 
 # Get the project root dir, which is the parent parent dir of this
 cwd = os.getcwd()
-project_root = os.path.dirname(os.path.dirname(cwd))
-print("found the following files in %s" % project_root)
-for root, dirs, files in os.walk(project_root):
-    for filename in files:
-        print(filename)
+project_root = os.path.dirname(cwd)
+print("project root:", project_root)
+print("current working directory:", cwd)
 
 # Insert the project root dir as the first element in the PYTHONPATH.
 # This lets us ensure that the source package is imported, and that its
