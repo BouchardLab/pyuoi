25dcaa0f7373eaac16e2187231a4691355f8e401
Andrew Tritt <ajtritt@lbl.gov>
2019-09-18 15:32:39 -0700

    append to sys path so sphinx autodoc works

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,14 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+# Get the project root dir, which is the parent parent dir of this
+cwd = os.getcwd()
+project_root = os.path.dirname(os.path.dirname(cwd))
+
+# Insert the project root dir as the first element in the PYTHONPATH.
+# This lets us ensure that the source package is imported, and that its
+# version is used.
+sys.path.insert(0, project_root)
 
 
 # -- Project information -----------------------------------------------------
