47513dd8624f0e47e1d2dff176e839611429c64a
Jan Balewski <janstar1122@gmail.com>
2025-03-13 13:50:58 -0700

    ok

--- causal_net/toys/uoi_var_mpi_checkBig.py
+++ causal_net/toys/uoi_var_mpi_checkBig.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+#srun -n256 shifter --image $IMG ./uoi_var_mpi_checkBig.py
+
 import sys,os
-print('M: MPI loaded')
+#print('M: MPI loaded')
 from pyuoi.linear_model import *
 sys.path.append(os.path.abspath("../../"))
 from examples.var_utils import *
@@ -15,28 +17,33 @@ from time import time
 # boolean for whether comparing fitting result from single vs multiple process
 compare = False
 
-n_features = 30
+n_features = 25
 n_samples = 1000
-lag = 10
-
-# srun -n 64 python uoi_var_mpi_check.py
+#lag = 1 ; rad=0.98
+lag = 2 ; rad=0.70
+lag = 3 ; rad=0.30
+lag = 4 ; rad=0.20
+#lag = 5 ; rad=0.20
 
 data, transition_matrices, cov = generate_sparse_stationary_var_process(
     n_features,
     n_samples,
     lag=lag,
     sparsity=0.5,
-    spectral_radius=0.98,  # Ensures stationarity
+    spectral_radius=rad,
     process_type='gaussian',
     random_state = 42
 
 )
-print('data generated nFeat=%d, nSamp=%d'%(n_features,n_samples))
+
+comm = MPI.COMM_WORLD
+num_ranks = comm.Get_size()
+if  comm.rank == 0 :
+    print('data generated nFeat=%d, nSamp=%d  lag=%d numRanks=%d'%(n_features,n_samples,lag,num_ranks), flush = True)
 dense_matrices = [M.toarray() for M in  transition_matrices]
 # vecortized the ground truth transition matrices
 B_truth = np.vstack([m.T for m in dense_matrices]).T.flatten()     
 X,Y = vectorization(data, lag)
-
 
 if compare:
     #fitting with single process
@@ -46,7 +53,6 @@ if compare:
 
 
 #fitting with multiple processes
-comm = MPI.COMM_WORLD
 
 uoi_lasso = UoI_Lasso(n_real_features = n_features, fit_VAR = True, random_state=42,comm = comm)
 
@@ -58,7 +64,7 @@ end = time()
 B_model_mpi = uoi_lasso.coef_
 
 if  comm.rank == 0 :
-    print("Fitting complete in "+str(end - start)+" seconds.", flush = True)
+    print("Fitting complete in %.1f sec  numRank=%d"%((end - start),num_ranks), flush = True)
 
 if compare:
     print(np.allclose(B_model, B_model_mpi))
