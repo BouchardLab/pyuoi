aa99651393cd4dd978ab03a859277d9da513af11
Jan Balewski <janstar1122@gmail.com>
2025-05-25 18:50:29 -0700

    fix bug

--- src/pyuoi/linear_model/admm_mpi.py
+++ src/pyuoi/linear_model/admm_mpi.py
@@ -415,7 +415,7 @@ class ADMM_Lasso:
         self.coef_ = z
         self.intercept_ = 0
         self.n_iter_ = None
-        self.rho_scale
+        self.rho_scaler
         
         
         return self
